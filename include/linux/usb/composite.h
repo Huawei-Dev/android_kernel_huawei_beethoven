468840b0c2029b9175a59daead2a12a40b7495d5
John Youn <John.Youn@synopsys.com>
2016-02-05 17:06:35 -0800

    usb: gadget: Update config for SuperSpeedPlus
    When a function is added to a configuration with usb_add_function(), the
    configuration speed flags are updated. These flags indicate for which
    speeds the configuration is valid for.
    
    This patch adds a flag in the configuration for SuperSpeedPlus and
    also updates this based on the existence of ssp_descriptors.
    
    Signed-off-by: John Youn <johnyoun@synopsys.com>
    Signed-off-by: Felipe Balbi <balbi@kernel.org>

--- include/linux/usb/composite.h
+++ include/linux/usb/composite.h
@@ -343,6 +343,7 @@ struct usb_configuration {
 	unsigned		superspeed:1;
 	unsigned		highspeed:1;
 	unsigned		fullspeed:1;
+	unsigned		superspeed_plus:1;
 	struct usb_function	*interface[MAX_CONFIG_INTERFACES];
 };
 
