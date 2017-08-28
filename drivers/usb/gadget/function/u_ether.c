d3e3ee4de608c63090653e6e0e1dd56f4c6baf99
Hemant Kumar <hemantk@codeaurora.org>
2017-08-28 13:26:06 -0700

    usb: gadget: u_ether: Add NULL check in helper APIs
    APIs being called as part of configfs attributes require
    to have NULL check for network device pointer. As user
    can call show and store call back even if network device
    is not allocated which results in to NULL pointer dereference.
    
    For example: In non-NCM composition user tries
    cat config/usb_gadget/g1/functions/ncm.0/ifname
    
    Change-Id: Ia4a1c6bec6d20e41886c952b0365419f2a8effcb
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/usb/gadget/function/u_ether.c
+++ drivers/usb/gadget/function/u_ether.c
@@ -1231,6 +1231,9 @@ int gether_set_dev_addr(struct net_device *net, const char *dev_addr)
 	struct eth_dev *dev;
 	u8 new_addr[ETH_ALEN];
 
+	if (!net)
+		return -ENODEV;
+
 	dev = netdev_priv(net);
 	if (get_ether_addr(dev_addr, new_addr))
 		return -EINVAL;
@@ -1243,6 +1246,9 @@ int gether_get_dev_addr(struct net_device *net, char *dev_addr, int len)
 {
 	struct eth_dev *dev;
 
+	if (!net)
+		return -ENODEV;
+
 	dev = netdev_priv(net);
 	return get_ether_addr_str(dev->dev_mac, dev_addr, len);
 }
@@ -1252,6 +1258,9 @@ int gether_set_host_addr(struct net_device *net, const char *host_addr)
 {
 	struct eth_dev *dev;
 	u8 new_addr[ETH_ALEN];
+
+	if (!net)
+		return -ENODEV;
 
 	dev = netdev_priv(net);
 	if (get_ether_addr(host_addr, new_addr))
@@ -1264,6 +1273,9 @@ EXPORT_SYMBOL_GPL(gether_set_host_addr);
 int gether_get_host_addr(struct net_device *net, char *host_addr, int len)
 {
 	struct eth_dev *dev;
+
+	if (!net)
+		return -ENODEV;
 
 	dev = netdev_priv(net);
 	return get_ether_addr_str(dev->host_mac, host_addr, len);
@@ -1297,6 +1309,9 @@ void gether_set_qmult(struct net_device *net, unsigned qmult)
 {
 	struct eth_dev *dev;
 
+	if (!net)
+		return;
+
 	dev = netdev_priv(net);
 	dev->qmult = qmult;
 }
@@ -1306,6 +1321,9 @@ unsigned gether_get_qmult(struct net_device *net)
 {
 	struct eth_dev *dev;
 
+	if (!net)
+		return -ENODEV;
+
 	dev = netdev_priv(net);
 	return dev->qmult;
 }
@@ -1313,6 +1331,9 @@ EXPORT_SYMBOL_GPL(gether_get_qmult);
 
 int gether_get_ifname(struct net_device *net, char *name, int len)
 {
+	if (!net)
+		return -ENODEV;
+
 	rtnl_lock();
 	strlcpy(name, netdev_name(net), len);
 	rtnl_unlock();
