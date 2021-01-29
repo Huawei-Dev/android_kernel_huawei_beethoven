fa6511f3d7dab5335112752662fe474d2304a842
darkhz <kmachanwenw@gmail.com>
2021-01-29 12:08:25 +0530

    sched/uclamp: Create a new uclamp.boosted tunable
    This complements the previous patch:
    sched/uclamp: Make uclamp_boosted() return proper boosted value
    
    We now can modify boost values for each taskgroup, just like in
    SchedTune, and uclamp_boosted() will correctly return the boost
    settings for the taskgroup.

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1820,6 +1820,11 @@ int cpu_uclamp_ls_write_u64(struct cgroup_subsys_state *css,
 				   struct cftype *cftype, u64 ls);
 u64 cpu_uclamp_ls_read_u64(struct cgroup_subsys_state *css,
 				  struct cftype *cft);
+
+int cpu_uclamp_boost_write_u64(struct cgroup_subsys_state *css,
+				   struct cftype *cftype, u64 boost);
+u64 cpu_uclamp_boost_read_u64(struct cgroup_subsys_state *css,
+				  struct cftype *cft);
 #endif
 
 
@@ -1946,6 +1951,12 @@ static struct cftype files[] = {
 		.flags = CFTYPE_NOT_ON_ROOT,
 		.read_u64 = cpu_uclamp_ls_read_u64,
 		.write_u64 = cpu_uclamp_ls_write_u64,
+	},
+	{
+		.name = "uclamp.boosted",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.read_u64 = cpu_uclamp_boost_read_u64,
+		.write_u64 = cpu_uclamp_boost_write_u64,
 	},
 #endif
 
