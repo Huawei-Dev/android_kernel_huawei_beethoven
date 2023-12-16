2b2456ed5285c3b1be5083c27ed01e82c7248814
Huawei-Dev <surdu_petru@yahoo.com>
2023-12-16 11:37:30 +0100

    revert commit: BACKPORT: irq: Make the irqentry text section unconditional :9412e45

--- arch/arm64/include/asm/traps.h
+++ arch/arm64/include/asm/traps.h
@@ -35,11 +35,18 @@ struct undef_hook {
 void register_undef_hook(struct undef_hook *hook);
 void unregister_undef_hook(struct undef_hook *hook);
 
+#ifdef CONFIG_FUNCTION_GRAPH_TRACER
 static inline int __in_irqentry_text(unsigned long ptr)
 {
 	return ptr >= (unsigned long)&__irqentry_text_start &&
 	       ptr < (unsigned long)&__irqentry_text_end;
 }
+#else
+static inline int __in_irqentry_text(unsigned long ptr)
+{
+	return 0;
+}
+#endif
 
 static inline int in_exception_text(unsigned long ptr)
 {
